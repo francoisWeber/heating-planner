54434fee7ac50c41a97453cde11099c96f29bb56
François Weber <f.weber@qwant.com>
2023-08-30 23:10:24 +0200

    [FEA] Built hypercube gathering primary and secondary ata

--- build_hypercube.py
+++ build_hypercube.py
@@ -8,17 +8,25 @@ def go(
     metadata_path: str = typer.Option(
         "/Users/f.weber/tmp-fweber/heating/metadata.json"
     ),
+    metadata_aux_path: str = typer.Option(
+        "/Users/f.weber/tmp-fweber/heating/metadata_aux.json"
+    ),
     output_path: str = typer.Option(
-        "/Users/f.weber/tmp-fweber/heating/processed/hypercube.npy"
+        "/Users/f.weber/tmp-fweber/heating/processed/hypercube2.npz"
     ),
 ):
+    # primary data
     df = pd.read_json(metadata_path)
-
     slices = [np.load(fpath) for fpath in df.fpath_array]
     log.info(f"Gathered {len(slices)} maps")
     hypercube = np.stack(slices, -1)
+    # aux data
+    df_aux = pd.read_json(metadata_aux_path)
+    slices_aux = [np.load(fpath) for fpath in df_aux.fpath_array]
+    log.info(f"Gathered {len(slices_aux)} aux maps")
+    hypercube_aux = np.stack(slices_aux, -1)
 
-    np.save(output_path, hypercube)
+    np.savez(output_path, map=hypercube, aux=hypercube_aux)
 
 
 if __name__ == "__main__":
