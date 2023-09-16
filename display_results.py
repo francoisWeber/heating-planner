b9d75519b930ac7f90fe874e5f18f47c29583eda
François Weber <f.weber@qwant.com>
2023-09-16 18:50:16 +0200

    [FEA] Added creds

--- display_results.py
+++ display_results.py
@@ -5,9 +5,21 @@ from heating_planner.utils import load_pil_from_anywhere
 from streamlit_image_coordinates import streamlit_image_coordinates
 import numpy as np
 import json
+import yaml
+import streamlit_authenticator as stauth
 
 st.set_page_config(layout="wide")
 st.title("Heating planner")
+
+with open("./credentials.yaml") as f:
+    config = yaml.load(f, Loader=yaml.loader.SafeLoader)
+authenticator = stauth.Authenticate(
+    config["credentials"],
+    config["cookie"]["name"],
+    config["cookie"]["key"],
+    config["cookie"]["expiry_days"],
+    config["preauthorized"],
+)
 
 
 @st.cache_data
@@ -31,33 +43,40 @@ def run(
     viable_path,
     hypercube_path,
 ):
-    with st.expander("Select a reference location"):
-        if "map_clicked_xy" not in st.session_state:
-            st.session_statemap_clicked_xy = None
-        base_map = st_load_pil_from_anywhere(basemap_path)
-        streamlit_image_coordinates(base_map, key="map_clicked_xy")
+    name, authentication_status, username = authenticator.login("Login", "main")
+    if authentication_status:
+        st.toast(f"Welcome {name} :smiley: ")
+        with st.expander("Select a reference location"):
+            if "map_clicked_xy" not in st.session_state:
+                st.session_statemap_clicked_xy = None
+            base_map = st_load_pil_from_anywhere(basemap_path)
+            streamlit_image_coordinates(base_map, key="map_clicked_xy")
 
-    with st.container():
-        display2.render(
-            metadata_path,
-            metadata_aux_path,
-            viable_path,
-            hypercube_path,
-        )
+        with st.container():
+            display2.render(
+                metadata_path,
+                metadata_aux_path,
+                viable_path,
+                hypercube_path,
+            )
 
-    with st.sidebar:
-        st.download_button(
-            "Download map",
-            display2.save_fig(),
-            file_name="score.png",
-            mime="image/png",
-        )
-        st.download_button(
-            "Download params",
-            json.dumps(display2.extract_params()),
-            mime="application/json",
-            file_name="params.json",
-        )
+        with st.sidebar:
+            st.download_button(
+                "Download map",
+                display2.save_fig(),
+                file_name="score.png",
+                mime="image/png",
+            )
+            st.download_button(
+                "Download params",
+                json.dumps(display2.extract_params()),
+                mime="application/json",
+                file_name="params.json",
+            )
+    elif authentication_status is False:
+        st.error("Incorrect login :warning:")
+    elif authentication_status is None:
+        st.warning("Enter credentials :upside_down_face: ")
 
 
 if __name__ == "__main__":
